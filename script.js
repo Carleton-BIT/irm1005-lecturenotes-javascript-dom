660ee02b8b62cbc37fd698b21463d1e6f7a13e7c
James Brunet <jamesbrunet@cunet.carleton.ca>
2025-11-03 12:30:39 -0500

    adding elements to the list

--- script.js
+++ script.js
@@ -2,4 +2,17 @@ let heading = document.querySelector("h1")
 heading.style.fontSize = "10px"
 
 const listElements = document.querySelectorAll("li");
-console.log(listElements)
+
+listElements.forEach(
+    element => {
+        element.textContent = 'One Of Us';
+    }
+)
+
+const newListElement = document.createElement("li")
+newListElement.textContent = 'James is the best'
+
+const myList = document.querySelector("ul")
+
+myList.append(newListElement)
+
