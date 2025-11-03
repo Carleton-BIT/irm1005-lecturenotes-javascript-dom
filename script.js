5de47d0093d4a740b9aebc4e461b4999509d5af9
James Brunet <jamesbrunet@cunet.carleton.ca>
2025-11-03 12:19:47 -0500

    add some JS

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("Hello World")
+let heading = document.querySelector("h1")
+heading.style.fontSize = "10px"
+
+const listElements = document.querySelectorAll("li");
+console.log(listElements)
